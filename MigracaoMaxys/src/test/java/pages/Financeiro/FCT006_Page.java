3004cab7add71cfc932483eece7ebfffeb072438
tielen.lima <tielen.lima@maxiconsystems.com.br>
2024-12-19 13:53:26 -0300

    Fix: Feature, Step, Page - FCT006

--- MigracaoMaxys/src/test/java/pages/Financeiro/FCT006_Page.java
+++ MigracaoMaxys/src/test/java/pages/Financeiro/FCT006_Page.java
@@ -1,80 +1,80 @@
-package pages.Financeiro;//package pages.Financeiro;
-
-import org.openqa.selenium.WebDriver;
-import pages.BasePage;
-
-public class FCT006_Page extends BasePage {
-    WebDriver driver;
-
-    // Strings Parâmetros de Consulta de Processos Bloqueados
-    String inputEmpresaXpath = "//*[@id='controle-cdEmpresa']/div/input";
-    String btnConsultarXpath = "//*[@id='controle-btConsulta']/button/div";
-    String inputDataPagamentoXpath = "//*[@id='controle-dtPagamento']/div/div/input";
-
-    // String Observação
-    String inputObservacaoXpath = "//*[@id='processocaixa-dsObservacao']/div/textarea";
-
-    // Botões - Processo Bloqueados
-    String btnAtualizaPXpath = "//*[@id='processocaixa-btnAutoriza']/button/div";
-    String btnRecusarXpath = "//*[@id='processocaixa-btnRecusa']/button/div";
-    String btnDetalhesXpath = "//*[@id='processocaixa-btnDetalhe']/button/div";
-    String btnPerfilXpath = "//*[@id='processocaixa-btnPerfil']/button/div";
-    String brnCreditoXpath = "//*[@id='cliforcredito-btnCredito']/button/div";
-    String btnCreditoContaXpath = "//*[@id='processocaixa-btnCreditoemconta']/button/div";
-    String btnContaOrigXpath = "//*[@id='processocaixa-btnOrigem']/button/div";
-    String btnDossieXpath = "//*[@id='processocaixa-btnDossie']/button/div";
-
-    // Globais
-    String inputXpathFim = "']/div/input";
-    String selectXpathFim = "']/div/select";
-    String descricaoFaker;
-    int primeiraLinhaEmBranco;
-
-    public FCT006_Page(WebDriver driver) {
-        super(driver);
-        this.driver = driver;
-    }
-
-    // Inicialização da primeiraLinhaEmBranco
-    public void setPrimeiraLinhaEmBranco() { primeiraLinhaEmBranco = encontrarPrimeiraLinhaEmBrancoNoGrid(driver);}
-
-    // Garantindo que a primeiraLinhaEmBranco seja inicializada apenas uma vez
-    private void verificarOuSetarPrimeiraLinhaEmBranco() {
-        if (primeiraLinhaEmBranco != 0) {setPrimeiraLinhaEmBranco();}
-    }
-
-    public void informarEmpresaFeatureFCT006(String empresa) {
-        clicarElementoByXpathNVezes(inputEmpresaXpath,2);
-        preencherElementoByXpath(inputEmpresaXpath, empresa);
-    }
-
-    public void realizarAConsultaFeatureFCT006() {
-        clicarElementoByXpathNVezes(btnConsultarXpath,2);
-        esperarMilissegundos(5000); // espera necessária
-    }
-
-    public void informaDataPagamentoFeatureFCT006(String dataPagamento) {
-        clicarElementoByXpathNVezes(inputDataPagamentoXpath,2);
-        preencherElementoByXpath(inputDataPagamentoXpath, dataPagamento);
-    }
-
-    public void selecionarOProcessoFeatureFCT006() {
-        verificarOuSetarPrimeiraLinhaEmBranco();
-
-        String chkProcessoXpath = "//*[@id='processocaixa-cbProcesso" + primeiraLinhaEmBranco + "']/div/button";
-
-        testMarcaDesmarcaCheckBox("marcar", chkProcessoXpath);
-    }
-
-    public void informarObservaçãoFeatureFCT006() {
-        descricaoFaker = faker.color().name() + " teste";
-
-        clicarElementoByXpathNVezes(inputObservacaoXpath,2);
-        preencherElementoByXpath(inputObservacaoXpath, descricaoFaker);
-    }
-
-    // Ajusar quando liberar a Base
-    public void autorizaProcessoFeatureFCT006() {
-        clicarElementoByXpathNVezes(inputDataPagamentoXpath,2);
-    }
-}
+//package pages.Financeiro;//package pages.Financeiro;
+//
+//import org.openqa.selenium.WebDriver;
+//import pages.BasePage;
+//
+//public class FCT006_Page extends BasePage {
+//    WebDriver driver;
+//
+//    // Strings Parâmetros de Consulta de Processos Bloqueados
+//    String inputEmpresaXpath = "//*[@id='controle-cdEmpresa']/div/input";
+//    String btnConsultarXpath = "//*[@id='controle-btConsulta']/button/div";
+//    String inputDataPagamentoXpath = "//*[@id='controle-dtPagamento']/div/div/input";
+//
+//    // String Observação
+//    String inputObservacaoXpath = "//*[@id='processocaixa-dsObservacao']/div/textarea";
+//
+//    // Botões - Processo Bloqueados
+//    String btnAtualizaPXpath = "//*[@id='processocaixa-btnAutoriza']/button/div";
+//    String btnRecusarXpath = "//*[@id='processocaixa-btnRecusa']/button/div";
+//    String btnDetalhesXpath = "//*[@id='processocaixa-btnDetalhe']/button/div";
+//    String btnPerfilXpath = "//*[@id='processocaixa-btnPerfil']/button/div";
+//    String brnCreditoXpath = "//*[@id='cliforcredito-btnCredito']/button/div";
+//    String btnCreditoContaXpath = "//*[@id='processocaixa-btnCreditoemconta']/button/div";
+//    String btnContaOrigXpath = "//*[@id='processocaixa-btnOrigem']/button/div";
+//    String btnDossieXpath = "//*[@id='processocaixa-btnDossie']/button/div";
+//
+//    // Globais
+//    String inputXpathFim = "']/div/input";
+//    String selectXpathFim = "']/div/select";
+//    String descricaoFaker;
+//    int primeiraLinhaEmBranco;
+//
+//    public FCT006_Page(WebDriver driver) {
+//        super(driver);
+//        this.driver = driver;
+//    }
+//
+//    // Inicialização da primeiraLinhaEmBranco
+//    public void setPrimeiraLinhaEmBranco() { primeiraLinhaEmBranco = encontrarPrimeiraLinhaEmBrancoNoGrid(driver);}
+//
+//    // Garantindo que a primeiraLinhaEmBranco seja inicializada apenas uma vez
+//    private void verificarOuSetarPrimeiraLinhaEmBranco() {
+//        if (primeiraLinhaEmBranco != 0) {setPrimeiraLinhaEmBranco();}
+//    }
+//
+//    public void informarEmpresaFeatureFCT006(String empresa) {
+//        clicarElementoByXpathNVezes(inputEmpresaXpath,2);
+//        preencherElementoByXpath(inputEmpresaXpath, empresa);
+//    }
+//
+//    public void realizarAConsultaFeatureFCT006() {
+//        clicarElementoByXpathNVezes(btnConsultarXpath,2);
+//        esperarMilissegundos(5000); // espera necessária
+//    }
+//
+//    public void informaDataPagamentoFeatureFCT006(String dataPagamento) {
+//        clicarElementoByXpathNVezes(inputDataPagamentoXpath,2);
+//        preencherElementoByXpath(inputDataPagamentoXpath, dataPagamento);
+//    }
+//
+//    public void selecionarOProcessoFeatureFCT006() {
+//        verificarOuSetarPrimeiraLinhaEmBranco();
+//
+//        String chkProcessoXpath = "//*[@id='processocaixa-cbProcesso" + primeiraLinhaEmBranco + "']/div/button";
+//
+//        testMarcaDesmarcaCheckBox("marcar", chkProcessoXpath);
+//    }
+//
+//    public void informarObservaçãoFeatureFCT006() {
+//        descricaoFaker = faker.color().name() + " teste";
+//
+//        clicarElementoByXpathNVezes(inputObservacaoXpath,2);
+//        preencherElementoByXpath(inputObservacaoXpath, descricaoFaker);
+//    }
+//
+//    // Ajusar quando liberar a Base
+//    public void autorizaProcessoFeatureFCT006() {
+//        clicarElementoByXpathNVezes(inputDataPagamentoXpath,2);
+//    }
+//}
