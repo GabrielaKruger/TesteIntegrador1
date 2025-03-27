3b2081e9fdbf38889e3260aa15b602270f890f2d
henrique.silva <henrique.silva@maxiconsystems.com.br>
2025-03-27 18:09:55 -0300

    Fix: ANV054 Step - VFS002 Feature, Page

--- MigracaoMaxys/src/test/java/pages/Faturamento/VFS/VFS002_Page.java
+++ MigracaoMaxys/src/test/java/pages/Faturamento/VFS/VFS002_Page.java
@@ -1,9 +1,6 @@
 package pages.Faturamento.VFS;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
